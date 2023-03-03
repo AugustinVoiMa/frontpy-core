d2abdbafaa579f9919c0b9dceea04a5c77790876
Augustin <augustinvoima@gmail.com>
2023-03-03 21:27:44 +0100

    fix issue for nested layouts

--- src/frontpy_core/core/views/layouts/layout.py
+++ src/frontpy_core/core/views/layouts/layout.py
@@ -7,9 +7,10 @@ from frontpy_core.core.views.view import View, AbstractView
 
 class Layout(View, ABC):
     def __init__(self, parent: Union[AbstractFrameController, AbstractView], **kw_attrs):
-        super(Layout, self).__init__(None, **kw_attrs)
         if isinstance(parent, AbstractFrameController):
             self._root = parent
+            parent = None
+        super(Layout, self).__init__(parent, **kw_attrs)
 
     @property
     def root(self):
